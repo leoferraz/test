1c2a353cda84df79064130a472cce31f887315ef
leoferraz <leo1806@gmail.com>
2023-06-13 10:15:48 -0300

    Adicionando teste de integracao

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+import sqlite3
+
 def add_numbers(a, b):
     return a + b
+
+def get_user_by_id(user_id):
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+
+    c.execute(f"SELECT * FROM users WHERE id={user_id}")
+
+    user = c.fetchone()
+    conn.close()
+    return user
 
--- test_app.py
+++ test_app.py
@@ -1,10 +1,22 @@
-import unittest
-from app import add_numbers
+import pytest
+import sqlite3
+from app import get_user_by_id
 
-class TestApp(unittest.TestCase):
-    def test_add_numbers(self):
-        self.assertEqual(add_numbers(1, 2), 3)
+def test_get_user_by_id(mocker):
+    # Substitui a função connect do módulo sqlite3 para retornar um objeto mock
+    mock_conn = mocker.patch('sqlite3.connect', return_value=mocker.Mock())
+    
+    # Define a resposta para a chamada fetchone do cursor
+    mock_conn.return_value.cursor.return_value.fetchone.return_value = ('1', 'John Doe')
 
-if __name__ == '__main__':
-    unittest.main()
+    user = get_user_by_id(1)
+
+    # Verifica se a função connect foi chamada com o nome correto do banco de dados
+    sqlite3.connect.assert_called_with('users.db')
+
+    # Verifica se o cursor executou a consulta SQL correta
+    mock_conn.return_value.cursor.return_value.execute.assert_called_with('SELECT * FROM users WHERE id=1')
+
+    # Verifica se a função retornou os dados corretos
+    assert user == ('1', 'John Doe')
 
